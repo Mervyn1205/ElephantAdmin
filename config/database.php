66b2030b652b9719bfd134e2c89507670398035f
Mervyn <404245127@qq.com>
2018-10-24 00:33:25 +0800

    update config

--- config/database.php
+++ config/database.php
@@ -15,21 +15,21 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => Env::get('DATABASE.DEFAULT_HOST', '127.0.0.1'),
     // 数据库名
-    'database'        => 'admin',
+    'database'        => Env::get('DATABASE.DEFAULT_DATABASE', ''),
     // 用户名
-    'username'        => 'root',
+    'username'        => Env::get('DATABASE.DEFAULT_USERNAME', 'root'),
     // 密码
-    'password'        => 'root',
+    'password'        => Env::get('DATABASE.DEFAULT_PASSWORD', ''),
     // 端口
-    'hostport'        => '3306',
+    'hostport'        => Env::get('DATABASE.DEFAULT_PORT', '3306'),
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
     'params'          => [],
     // 数据库编码默认采用utf8
-    'charset'         => 'utf8',
+    'charset'         => Env::get('DATABASE.DEFAULT_CHARSET', 'utf8mb4'),
     // 数据库表前缀
     'prefix'          => 't_',
     // 数据库调试模式
